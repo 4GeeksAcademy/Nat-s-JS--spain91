0cca3d8045cc27ec845caec1a44ee3314132a326
Natalia Manzano Carnerero <mcn.sdevelop@gmail.com>
2024-11-14 14:37:23 +0000

    generador excusas

--- src/app.js
+++ src/app.js
@@ -2,11 +2,31 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/excusas.jpg";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-  console.log("Hola spain 91");
+
+
+function generarExcusa() {
+
+  let quien = ["el perro", "el gato", "un dinisaurio", "un alien"];
+  let accion = ["se comio", "se llevo", "hizo pedazos", "destruyo"];
+  let objeto = ["mis deberes", "mi telefono", "el coche", "mi dinero"];
+  let lugar = ["antes de clase", "mientras dormia", "cundo fui a la compra", "mientras comia"];
+
+
+  let quienIndex = Math.floor(Math.random() * quien.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let objetoIndex = Math.floor(Math.random() * objeto.length);
+  let lugarIndex = Math.floor(Math.random() * lugar.length);
+
+  document.querySelector("#excuse").innerHTML = quien[quienIndex] + " "
+    + accion[accionIndex] + " " + objeto[objetoIndex] + " " + lugar[lugarIndex];
 };
+
+
+window.onload = function () {
+  generarExcusa();
+}
+
+
+
